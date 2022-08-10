21ee2348b6dd216ba34d01d250664e286915be61
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-10 08:49:07 -0400

    Update AssemblyBasicOperations.sol
    Added public method/function assemblyIf() , which is a view only function that returns a variable result(uint).

--- AssemblyBasicOperations.sol
+++ AssemblyBasicOperations.sol
@@ -21,4 +21,18 @@ contract AssemblyBasicOperations {
             result = num2;
         }
     }
+    
+     function assemblyIf(uint num1, uint num2) public view returns (uint result) {
+        assembly {
+            jumpi(num1Greater, gt(num1, num2))
+            jump(num2Greater)
+            
+            num1Greater:
+                result := num1
+                jump(end)
+            num2Greater:
+                result := num2
+            end:
+        }
+    }
 }
