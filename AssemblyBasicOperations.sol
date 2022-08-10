3ad29fda81b23cb02e14da3032850d45941e079d
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-10 08:47:12 -0400

    Update AssemblyBasicOperations.sol
    Added public method/function assemblyLoop() , a view only function that returns a variable result(uint).

--- AssemblyBasicOperations.sol
+++ AssemblyBasicOperations.sol
@@ -4,4 +4,13 @@ contract AssemblyBasicOperations {
             result += i;
         }
     }
+    function assemblyLoop() public view returns (uint result) {
+       assembly {
+           let i := 0
+           loop:
+               result := add(result, i)
+               i := add(i, 1)
+               jumpi(loop, lt(i, 10))
+        }
+    }
 }
