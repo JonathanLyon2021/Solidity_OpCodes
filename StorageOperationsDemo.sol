635e2534927b5994b4a4ce1479515731157d4ce1
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-11 10:26:01 -0400

    Update StorageOperationsDemo.sol

--- StorageOperationsDemo.sol
+++ StorageOperationsDemo.sol
@@ -7,4 +7,10 @@ contract StorageOperationsDemo {
             sstore(add(x_slot, x_offset), value)
         }
     }
+    
+    function setY(uint value) public {
+        assembly {
+            sstore(add(y_slot, y_offset), value)
+        }
+    }
 }
