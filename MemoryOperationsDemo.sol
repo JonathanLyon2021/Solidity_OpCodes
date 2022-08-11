063ef2c175f1b38ff1f6f7d0feae69c4235549dc
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-11 09:59:57 -0400

    Update MemoryOperationsDemo.sol

--- MemoryOperationsDemo.sol
+++ MemoryOperationsDemo.sol
@@ -38,6 +38,17 @@ contract MemoryOperationsDemo {
               mstore(0x0, 0x20) // store the data type in mem[0]. 0x20 means one-dimensional dynamic array
             mstore(0x20, len) // store the length in mem[1]
             
+             loop:
+                element := mload(elAddress)
+                element := mul(element, 2)
+                
+                // store element i in mem[2 + i]
+                mstore(add(0x40, mul(i, 0x20)), element)
+                
+                elAddress := add(elAddress, 0x20)
+                
+                i := add(i, 1)
+                jumpi(loop, lt(i, len))
                     
             // return mem[0..len+2]
             return(0x0, add(0x40, mul(len, 0x20)))
