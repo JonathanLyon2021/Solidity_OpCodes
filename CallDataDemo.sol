477d2703f7fbf3126be325d8ba82b22156ec4e8e
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-12 23:36:53 -0400

    Create CallDataDemo.sol

---
+++ CallDataDemo.sol
@@ -0,0 +1,10 @@
+contract CalldataDemo {
+    function sumArrayPublic(uint[] arr) public view returns (uint) {
+        uint sum = 0;
+        for (uint i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        
+        return sum;
+    }
+}
