c062ed585f2befca5f5538918b348716de459639
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-12 23:40:08 -0400

    Update CallDataDemo.sol

--- CallDataDemo.sol
+++ CallDataDemo.sol
@@ -7,4 +7,13 @@ contract CalldataDemo {
         
         return sum;
     }
+    
+    function sumArrayExternal(uint[] arr) external view returns (uint) {
+        uint sum = 0;
+        for (uint i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        
+        return sum;
+    }
 }
