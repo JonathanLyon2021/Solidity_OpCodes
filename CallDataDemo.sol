2ae2799519508d8511be36fe6a07f76870a12223
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-19 01:41:53 -0400

    Update CallDataDemo.sol

--- CallDataDemo.sol
+++ CallDataDemo.sol
@@ -22,6 +22,7 @@ contract CalldataDemo {
         return arr;
     }
     
+    ////extra functionality
     // function scaleArrayExternal(uint[] arr) external returns (uint[]) {
     //     for (uint i = 0; i < arr.length; i++) {
     //         // This will not work. calldata is read-only in external functions
